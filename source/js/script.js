7924edb746c3845570ae4e2379050e4e849f4d08
Sigrdriva <gavrilenko.m.e@gmail.com>
2023-06-23 00:36:17 +0300

    Добавляет стили хедера и хероблока в мобильной версии

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,25 @@
-/* в этот файл добавляет скрипты*/
+let siteList = document.querySelector('.site-list');
+let navToggle = document.querySelector('.main-nav__toggle');
+
+siteList.classList.remove('site-list--nojs');
+navToggle.classList.remove('main-nav__toggle--nojs');
+
+navToggle.addEventListener('click', function () {
+  if (siteList.classList.contains('site-list--closed')) {
+    siteList.classList.remove('site-list--closed');
+    siteList.classList.add('site-list--opened');
+  } else {
+    siteList.classList.add('site-list--closed');
+    siteList.classList.remove('site-list--opened');
+  }
+});
+
+navToggle.addEventListener('click', function () {
+  if (navToggle.classList.contains('main-nav__toggle--closed')) {
+    navToggle.classList.remove('main-nav__toggle--closed');
+    navToggle.classList.add('main-nav__toggle--opened');
+  } else {
+    navToggle.classList.add('main-nav__toggle--closed');
+    navToggle.classList.remove('main-nav__toggle--opened');
+  }
+});
