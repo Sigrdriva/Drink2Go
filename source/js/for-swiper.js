7da272894a4bf28cb460d75a37a0bac982fedbe2
Sigrdriva <gavrilenko.m.e@gmail.com>
2023-06-24 15:50:59 +0300

    Добавляет слайдер для мобильной версии

---
+++ source/js/for-swiper.js
@@ -0,0 +1,20 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+
+  // If we need pagination
+  // pagination: {
+  //   el: '.swiper-pagination',
+  // },
+
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-right',
+    prevEl: '.swiper-button-left',
+  },
+
+});
