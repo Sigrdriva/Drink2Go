925fa6b432cfa6afeab4cd78b8975477e7a4e344
Sigrdriva <gavrilenko.m.e@gmail.com>
2023-06-25 16:15:23 +0300

    Верстка ПК версии

--- source/js/for-swiper.js
+++ source/js/for-swiper.js
@@ -3,12 +3,26 @@ const swiper = new Swiper('.swiper', {
   loop: true,
 
   // If we need pagination
-  // pagination: {
-  //   el: '.swiper-pagination',
-  // },
 
-  scrollbar: {
-    el: ".swiper-scrollbar",
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'custom',
+    renderCustom: function(swiper, current, total) {
+      console.log (current);
+      console.log (total);
+      var progress = current / (total - 1) * 100;
+      console.log (progress);
+      var progressBarHTML = '';
+
+      for (var i = 1; i <= total; i++) {
+        var colorClass = (i == current) ? 'progress-bar-active' : 'progress-bar-inactive';
+        progressBarHTML += '<div class="progress-bar ' + colorClass + '"></div>';
+      }
+
+      return progressBarHTML;
+
+    }
+
   },
 
   // Navigation arrows
